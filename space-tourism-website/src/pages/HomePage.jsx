162455d7401552288d8632277f5318bcf215cc00
sashkajanevska <janevska_sashka@yahoo.com>
2024-08-09 22:42:16 +0200

    button function added

--- space-tourism-website/src/pages/HomePage.jsx
+++ space-tourism-website/src/pages/HomePage.jsx
@@ -1,17 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateTo = () => {
+    navigate("/destination/moon");
+  };
+
   return (
     <div className="home-container">
       <div className="home-content">
-        <h2>SO, YOU WANT TO TRAVEL TO</h2>
+        <h3>SO, YOU WANT TO TRAVEL TO</h3>
         <h1>SPACE</h1>
         <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
+          Let's face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
+          back, and relax because we'll give you a truly out of this world
           experience!!
         </p>
       </div>
-      <button className="home-btn">EXPLORE</button>
+      <button className="home-btn" onClick={navigateTo}>
+        EXPLORE
+      </button>
     </div>
   );
 }
