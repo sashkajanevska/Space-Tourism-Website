ca5ec51550f650df6fadaaeb021e4fdc18591414
sashkajanevska <janevska_sashka@yahoo.com>
2024-08-29 19:49:58 +0200

    images location fix

--- space-tourism-website/src/components/Header.jsx
+++ space-tourism-website/src/components/Header.jsx
@@ -14,30 +14,16 @@ export default function Header() {
 
   return (
     <div className="header">
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-        className="logo"
-      >
-        <g fill="none" fill-rule="evenodd">
-          <circle cx="24" cy="24" r="24" fill="#FFF" />
-          <path
-            fill="#0B0D17"
-            d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"
-          />
-        </g>
-      </svg> */}
       <img className="logo" src="/logo.svg" alt="logo" />
       <img
         className="nav-button active"
-        src="/src/images/shared/icon-hamburger.svg"
+        src="/images/shared/icon-hamburger.svg"
         alt="open-nav"
         onClick={toggleNav}
       />
       <img
         className="nav-button close"
-        src="/src/images/shared/icon-close.svg"
+        src="/images/shared/icon-close.svg"
         alt="close-nav"
         onClick={toggleNav}
       />
